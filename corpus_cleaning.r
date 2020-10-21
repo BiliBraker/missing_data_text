11be814694799a12c5135dee33246e6068b8c000
BiliBraker <soirkorob@gmail.com>
2020-10-21 10:01:19 +0200

    cleaning script

--- corpus_cleaning.r
+++ corpus_cleaning.r
@@ -4,9 +4,10 @@ library(tidyverse)
 library(tm)
 library(parallel)
 library(stringi)
+library(text2vec)
 library(jprep)
 
-jstor_corpus <- readRDS("C:/Users/soirk/Krisztian/Research/missing_data_paper/corpus_files/jstor_corpus_original.rds")
+jstor_corpus <- readRDS("C:/Users/soirk/Krisztian/Research/missing_data_paper/corpus_files/jstor_corpus.rds")
 ## cleaning
 
 numCores <- detectCores()
@@ -15,12 +16,20 @@ clusterExport(cl, c(
   "tm_map", "removeWords",
   "remove_special", "removeNumbers",
   "removePunctuation", "stripWhitespace",
-  "stemDocument", "stri_trans_tolower"
+  "stemDocument", "stri_trans_tolower",
+  "DocumentTermMatrix", "findFreqTerms",
+  "word_tokenizer", "stri_replace_all_fixed",
+  "%notin%", "vocab",
+  "unfreq_term_remove",
+  "stripWhitespace", "stemDocument"
 ))
 tm_parLapply_engine(cl)
 
-jstor_corpus <- tm_map(jstor_corpus, removeWords, stopwords_new)
 jstor_corpus <- tm_map(jstor_corpus, content_transformer(stri_trans_tolower))
+
+### need to delete references
+
+jstor_corpus <- tm_map(jstor_corpus, removeWords, stopwords_new)
 jstor_corpus <- tm_map(jstor_corpus, latex_html_remove)
 jstor_corpus <- tm_map(jstor_corpus, removePunctuation)
 jstor_corpus <- tm_map(jstor_corpus, removeNumbers)
@@ -30,35 +39,17 @@ jstor_corpus <- tm_map(jstor_corpus, remove_special, "[^[:alnum:]]")
 jstor_corpus <- tm_map(jstor_corpus, remove_special, "[\r\n]")
 jstor_corpus <- tm_map(jstor_corpus, stripWhitespace)
 
-stopCluster(cl)
-
-jstor_corpus %>% saveRDS(., "./corpus_files/jstor_corpus_cleaned.rds")
+#jstor_corpus %>% saveRDS(., "./corpus_files/jstor_corpus_cleaned.rds")
 
 # Remove unfrequent and meaningless terms before stemming
 ## Creating DocumentTermMatrix with parallel
 
-cl <- makeCluster(numCores)
-clusterExport(cl, c("DocumentTermMatrix", "findFreqTerms"))
-tm_parLapply_engine(cl)
-
 dtm <- DocumentTermMatrix(jstor_corpus)
 unfreqterms <- findFreqTerms(dtm, 0, 5)
-stopCluster(cl)
 
 unfreqterms <- unfreqterms[which(unfreqterms %notin% vocab)]
 
-
-cl <- makeCluster(numCores)
-clusterExport(cl, c(
-  "tm_map", "word_tokenizer",
-  "stri_replace_all_fixed",
-  "%notin%", "vocab",
-  "unfreq_term_remover", "unfreqterms",
-  "stripWhitespace", "stemDocument"
-))
-tm_parLapply_engine(cl)
-
-jstor_corpus <- tm_map(jstor_corpus, content_transformer(unfreq_term_remover), unfreqterms)
+jstor_corpus <- tm_map(jstor_corpus, content_transformer(unfreq_term_remove), unfreqterms)
 jstor_corpus <- tm_map(jstor_corpus, stripWhitespace)
 jstor_corpus <- tm_map(jstor_corpus, stemDocument)
 
