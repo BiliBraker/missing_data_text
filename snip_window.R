b4d2d9da53ed8d47a30b382430bf159ad44d0e97
Krisztián Boros <51168364+BiliBraker@users.noreply.github.com>
2020-11-30 15:54:54 +0100

    creating the windows to snip out of from the text

---
+++ snip_window.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(tidytext)
+library(text2vec)
+
+jstor_df <- readRDS("/media/bilibraker/Maxtor/Krisz/Krisztian/Research/missing_data_paper/corpus_files/jstor_df_stem_trim_30_11.rds")
