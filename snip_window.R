0a277894864ce32f593f151cddee62e4e645e748
Krisztián Boros <51168364+BiliBraker@users.noreply.github.com>
2020-12-23 15:42:07 +0100

    script for extracting "snippets" from the texts

--- snip_window.R
+++ snip_window.R
@@ -18,18 +18,32 @@ text_tokens <- tokens(jstor_corpus)
 text_tokens <- as.tokens(text_tokens)
 
 dict <- dictionary(list(missing = c("miss", "missing"),
-                      imputation = c("imput", "impute")))
-
+                        imputation = c("imput", "impute")))
 
 text_tokens <- tokens_select(text_tokens, pattern = dict, selection = "keep", window = 5)
+text_tokens <- text_tokens %>% as.list()
 
-dfm <- dfm(text_tokens)
+jstor_df <- jstor_df %>% add_column(., text_tokens = NA)
+
+for (i in seq_along(text_tokens)){
+
+  jstor_df$text_tokens[i] <- text_tokens[i] %>% as.character()
+
+}
+
+jstor_df$text_tokens <- jstor_df$text_tokens %>% unlist()
+
+jstor_df$text_tokens <- jstor_df$text_tokens %>% stringi::stri_replace_all_fixed(., pattern = "c(" , replacement = "")
+jstor_df$text_tokens <- jstor_df$text_tokens %>% gsub(pattern = "[^A-Za-z0-9]", replacement = " ",x = .)
+jstor_df$text_tokens <- jstor_df$text_tokens %>% stringi::stri_replace_all_fixed(., pattern = '"' , replacement = "")
+
+#saveRDS(jstor_df, "/media/bilibraker/Maxtor/Krisz/Krisztian/Research/missing_data_paper/corpus_files/jstor_df_snipped.rds")
 
 ### data.table + text2vec
 setDT(jstor_df)
 setkey(jstor_df, id)
 
-it <- itoken(jstor_df$text,
+it <- itoken(jstor_df$text_tokens,
                    tokenizer = word_tokenizer,
                    ids = jstor_df$id,
                    progressbar = FALSE)
@@ -37,8 +51,3 @@ it <- itoken(jstor_df$text,
 vocabulary <- create_vocabulary(it)
 
 vocabulary  <- prune_vocabulary(vocabulary , term_count_min = 5L)
-
-
-text_tokens <- tokens(jstor_df$text)
-
-text_tokens <- as.tokens(text_tokens)
