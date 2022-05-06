2a09a5936eb3e9aa08c2566bc250e025c5783a8b
Krisztián Boros <soirkorob@gmail.com>
2022-05-06 19:51:54 +0900

    update

--- log_models.R
+++ log_models.R
@@ -9,49 +9,30 @@ data <- read_csv("~/research-sync/missing_data_paper/fasttext/4_all_levels/jstor
 data$discipline <- stri_replace_all_regex(data$discipline, pattern = " & ", replacement = " and ")
 data$pub_year_cat <- relevel(as_factor(data$pub_year_cat), "[1999,2005]")
 # pub_year as continous
-model <- glm(advanced ~ pub_year + discipline + pub_year * discipline, data = data, family = "binomial")
+model <- glm(advanced ~ pub_year + discipline, data = data, family = "binomial")
 summary(model)
 margins(model)
 ggpredict(model)
 ggpredict(model, "pub_year")
 ggpredict(model, "discipline")
 
-model1 <- glm(imputation ~ pub_year + discipline + pub_year * discipline, data = data, family = "binomial")
+model1 <- glm(imputation ~ pub_year + discipline, data = data, family = "binomial")
 summary(model1)
 margins(model1)
 ggpredict(model1)
 ggpredict(model1, "pub_year")
 ggpredict(model1, "discipline")
 
-model2 <- glm(deletion ~ pub_year + discipline + pub_year * discipline, data = data, family = "binomial")
+model2 <- glm(advanced ~ pub_year + discipline + pub_year * discipline, data = data, family = "binomial")
 summary(model2)
 
-# save the results
-stargazer(model, model1,
-  type = "html",
-  out = "~/research/missing_data_paper/paper/log_models.html",
-  title = "Logistic models",
-  dep.var.labels = c(
-    "Advanced Imputation",
-    "Imputation",
-    "Deletion"
-  )
-)
-
-# pub_year as categorical
-model3 <- glm(advanced ~ pub_year_cat + discipline + pub_year_cat * discipline, data = data, family = "binomial")
+model3 <- glm(imputation ~ pub_year + discipline + pub_year * discipline, data = data, family = "binomial")
 summary(model3)
 
-model4 <- glm(imputation ~ pub_year_cat + discipline + pub_year_cat * discipline, data = data, family = "binomial")
-summary(model4)
-
-model5 <- glm(deletion ~ pub_year_cat + discipline + pub_year_cat * discipline, data = data, family = "binomial")
-summary(model5)
-
 # save the results
-stargazer(model3, model4,
+stargazer(model, model1, model2, model3,
   type = "html",
-  out = "~/research/missing_data_paper/paper/log_models2.html",
+  out = "~/research/missing_data_paper/paper/log_models_4.html",
   title = "Logistic models",
   dep.var.labels = c(
     "Advanced Imputation",
@@ -59,3 +40,8 @@ stargazer(model3, model4,
     "Deletion"
   )
 )
+
+saveRDS(model, "./log_model.rds")
+saveRDS(model1, "./log_model1.rds")
+saveRDS(model2, "./log_model2.rds")
+saveRDS(model3, "./log_model3.rds")
